d538b12d061c3ff70c598810d78070fbb24d7fff
Marthenn <89301265+Marthenn@users.noreply.github.com>
2023-02-17 23:32:39 +0700

    chore: change the bot name

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -58,7 +58,7 @@ public class Main {
 
         Thread.sleep(1000);
         System.out.println("Registering with the runner...");
-        hubConnection.send("Register", token, "someone");
+        hubConnection.send("Register", token, "breve");
 
         //This is a blocking call
         hubConnection.start().subscribe(() -> {
